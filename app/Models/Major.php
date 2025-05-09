e27a197314cbb71aa28e188a2c5b1b7befbdc980
Muhammad Khoirul Anwarudin <dimaskechiel58@gmail.com>
2025-05-10 00:25:45 +0700

    feat: Added migrations and models for majors and study_programs tables

---
+++ app/Models/Major.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Major extends Model
+{
+    use HasFactory;
+
+    protected $table = 'majors';
+
+    protected $fillable = [
+        'major_name',
+    ];
+
+    protected $primaryKey = 'major_id';
+}
