a60fd9a2d706f64b629d86369e3b65952c564e1b
Muhammad Khoirul Anwarudin <dimaskechiel58@gmail.com>
2025-05-11 18:38:39 +0700

    feat: Added migration and model for achievements table

---
+++ app/Models/Achievement.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Achievement extends Model
+{
+    use HasFactory;
+
+    protected $table = 'achievements';
+
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'achievement_title',
+        'achievement_description',
+        'achievement_ranking',
+        'achievement_level',
+        'achievement_document',
+        'achievement_verified',
+    ];
+
+    protected $primaryKey = 'achievement_id';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'category_id');
+    }
+}
