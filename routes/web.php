31c544f4c6d95459ca93d69e217f0c09e687a832
Muhammad Khoirul Anwarudin <dimaskechiel58@gmail.com>
2025-05-12 09:15:57 +0700

    refactor: modify all migrations to rename tables and adjust relations

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
